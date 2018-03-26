4a4481b041b14544599be6af98b3e3cf85c8aec1
Thomas Coleno <thomas.coleno@cgi.com>
2018-03-26 11:40:14 +0200

    init controller

--- src/main/java/fr/openent/mediacentre/Mediacentre.java
+++ src/main/java/fr/openent/mediacentre/Mediacentre.java
@@ -1,5 +1,6 @@
 package fr.openent.mediacentre;
 
+import fr.openent.mediacentre.controller.MediacentreController;
 import org.entcore.common.http.BaseServer;
 
 public class Mediacentre extends BaseServer {
@@ -7,6 +8,7 @@ public class Mediacentre extends BaseServer {
 	@Override
 	public void start() {
 		super.start();
+		addController(new MediacentreController());
 	}
 
 }
---
+++ src/main/java/fr/openent/mediacentre/controller/MediacentreController.java
@@ -0,0 +1,11 @@
+package fr.openent.mediacentre.controller;
+
+import org.entcore.common.controller.ControllerHelper;
+
+public class MediacentreController extends ControllerHelper {
+
+
+    public MediacentreController() {
+        super();
+    }
+}
