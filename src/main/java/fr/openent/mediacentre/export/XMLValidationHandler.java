4228244cd171979296ec2e09f83492eaf64293da
Simon LEDUNOIS <simon.ledunois@gmail.com>
2019-08-07 14:31:02 +0200

    feat(GAR): Add xsd validation and email report

---
+++ src/main/java/fr/openent/mediacentre/export/XMLValidationHandler.java
@@ -0,0 +1,69 @@
+package fr.openent.mediacentre.export;
+
+import org.xml.sax.ErrorHandler;
+import org.xml.sax.SAXException;
+import org.xml.sax.SAXParseException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class XMLValidationHandler implements ErrorHandler {
+    private List<String> warnings;
+    private List<String> errors;
+    private List<String> fatalErrors;
+    private boolean valid;
+
+    public XMLValidationHandler () {
+        valid = true;
+        this.warnings = new ArrayList<>();
+        this.errors = new ArrayList<>();
+        this.fatalErrors = new ArrayList<>();
+    }
+
+    @Override
+    public void warning(SAXParseException exception) throws SAXException {
+        this.warnings.add(exception.toString());
+    }
+
+    @Override
+    public void error(SAXParseException exception) throws SAXException {
+        this.errors.add(exception.toString());
+        valid = false;
+    }
+
+    @Override
+    public void fatalError(SAXParseException exception) throws SAXException {
+        this.fatalErrors.add(exception.toString());
+        valid = false;
+    }
+
+    public boolean isValid() {
+        return valid;
+    }
+
+    public String report() {
+        StringBuilder report = new StringBuilder();
+        if (!warnings.isEmpty()) {
+            report.append("\n=== ").append(warnings.size()).append(" warnings found ===\n");
+            for (String warning: warnings) {
+                report.append(warning).append("\n");
+            }
+        }
+
+        if (!errors.isEmpty()) {
+            report.append("\n=== ").append(errors.size()).append(" errors found ===\n");
+            for (String error: errors) {
+                report.append(error).append("\n");
+            }
+        }
+
+        if (!fatalErrors.isEmpty()) {
+            report.append("\n=== ").append(fatalErrors.size()).append(" fatal errors found ===\n");
+            for (String fatalError: fatalErrors) {
+                report.append(fatalError).append("\n");
+            }
+        }
+
+        return report.toString();
+    }
+}
--- src/main/java/fr/openent/mediacentre/export/impl/ExportImpl.java
+++ src/main/java/fr/openent/mediacentre/export/impl/ExportImpl.java
@@ -1,18 +1,32 @@
 package fr.openent.mediacentre.export.impl;
 
 import fr.openent.mediacentre.export.ExportService;
+import fr.openent.mediacentre.export.XMLValidationHandler;
 import fr.openent.mediacentre.service.TarService;
 import fr.openent.mediacentre.service.impl.DefaultTarService;
 import fr.wseduc.webutils.Either;
+import fr.wseduc.webutils.data.FileResolver;
+import fr.wseduc.webutils.email.EmailSender;
 import io.vertx.core.Handler;
 import io.vertx.core.Vertx;
 import io.vertx.core.eventbus.EventBus;
 import io.vertx.core.eventbus.Message;
+import io.vertx.core.json.JsonArray;
 import io.vertx.core.json.JsonObject;
 import io.vertx.core.logging.Logger;
 import io.vertx.core.logging.LoggerFactory;
+import org.entcore.common.email.EmailFactory;
+import org.xml.sax.SAXException;
 
+import javax.xml.XMLConstants;
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.validation.Validator;
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 import static fr.openent.mediacentre.Mediacentre.CONFIG;
 import static fr.wseduc.webutils.Utils.handlerToAsyncHandler;
@@ -26,6 +40,7 @@ public class ExportImpl {
     private EventBus eb;
     private JsonObject config;
     private Vertx vertx;
+    private EmailSender emailSender;
 
     public ExportImpl(Vertx vertx, Handler<String> handler) {
         this.vertx = vertx;
@@ -34,11 +49,12 @@ public class ExportImpl {
         this.exportService = new ExportServiceImpl(config);
         this.tarService = new DefaultTarService();
         this.sftpGarConfig = config.getJsonObject("gar-sftp");
+        this.emailSender = new EmailFactory(vertx, config).getSender();
 
         this.exportAndSend(handler);
     }
     private void exportAndSend(Handler<String> handler) {
-        log.info("Start exportAndSend GAR (Generate xml files, compress to tar.gz, generate md5, send to GAR by sftp");
+        log.info("Start exportAndSend GAR (Generate xml files, XSD validation, compress to tar.gz, generate md5, send to GAR by sftp");
         try {
             emptyDIrectory(config.getString("export-path"));
         } catch (Exception e) {
@@ -48,6 +64,14 @@ public class ExportImpl {
         exportService.launchExport((Either<String, JsonObject> event1) -> {
             if (event1.isRight()) {
                 File directory = new File(config.getString("export-path"));
+                Map<String, Object> validationResult = validateXml(directory);
+                boolean isValid = (boolean) validationResult.get("valid");
+                if (!isValid) {
+                    log.info(validationResult.get("report"));
+                    sendReport((String) validationResult.get("report"));
+                    handler.handle("XSV VALIDATION ERROR");
+                    return;
+                }
                 log.info("Tar.GZ to Compress");
                 emptyDIrectory(config.getString("export-archive-path"));
                 tarService.compress(config.getString("export-archive-path"), directory, (Either<String, JsonObject> event2) -> {
@@ -103,6 +127,43 @@ public class ExportImpl {
                 handler.handle(e);
             }
         });
+    }
+
+    private void sendReport(String report) {
+        JsonArray recipients = config.getJsonArray("xsd-recipient-list", new JsonArray());
+        String subject = "[GAR][" + config.getString("host") + "] XSD Validation error";
+        for (int i = 0; i < recipients.size(); i++) {
+            String recipient = recipients.getString(i);
+            emailSender.sendEmail(null, recipient, null, null, subject, report, null, false, null);
+        }
+    }
+
+    private Map<String, Object> validateXml(File directory) {
+        Map<String, Object> result = new HashMap<>();
+        XMLValidationHandler errorHandler = new XMLValidationHandler();
+        try {
+            SchemaFactory factory = SchemaFactory.newInstance(XMLConstants.W3C_XML_SCHEMA_NS_URI);
+            String schemaPath = FileResolver.absolutePath("public/xsd");
+            Schema schema = factory.newSchema(new File(schemaPath + "/GAR-ENT.xsd"));
+            Validator validator = schema.newValidator();
+            validator.setErrorHandler(errorHandler);
+            String[] files = directory.list();
+            if (files == null) {
+                result.put("valid", true);
+                return result;
+            }
+            for (String f : files) {
+                File currentFile = new File(directory.getPath(), f);
+                validator.validate(new StreamSource(currentFile));
+            }
+        } catch (SAXException | IOException e) {
+            log.error("Error while validating xml", e);
+            result.put("valid", false);
+        } finally {
+            result.put("valid", errorHandler.isValid());
+            result.put("report", errorHandler.report());
+        }
+        return result;
     }
 
     //TODO prévenir les nullpointer ici
